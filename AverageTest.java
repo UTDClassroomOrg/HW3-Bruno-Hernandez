1ce115c4fc72b64f1b05128d490626a11f9d8762
BrunoHernandez12 <32974591+BrunoHernandez14@users.noreply.github.com>
2024-12-10 04:20:44 -0600

    Update AverageTest.java

--- AverageTest.java
+++ AverageTest.java
@@ -3,31 +3,31 @@ import static org.junit.Assert.*;
 
 public class AverageTest {
     @Test
-    public void testBasicCase() {
+    public void basicCase() {
         Average avg = new Average();
         assertEquals(4, avg.average(3, new int[]{2, 4, 6, 8}));
     }
 
     @Test
-    public void testKExceedsArrayLength() {
+    public void exceeds() {
         Average avg = new Average();
         assertEquals(2, avg.average(10, new int[]{1, 2, 3}));
     }
 
     @Test
-    public void testEmptyList() {
+    public void empty() {
         Average avg = new Average();
         assertEquals(0, avg.average(5, new int[]{}));
     }
 
     @Test
-    public void testSingleElement() {
+    public void single() {
         Average avg = new Average();
         assertEquals(42, avg.average(1, new int[]{42}));
     }
 
     @Test
-    public void testZeroK() {
+    public void zero() {
         Average avg = new Average();
         assertEquals(0, avg.average(0, new int[]{1, 2, 3}));
     }
